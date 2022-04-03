3c66ddbc56a58726b463ebf9ad0c2478f0f7a76a
Sohail Ansari <sohail.fladdra@gmail.com>
2022-04-03 09:00:53 +0530

    codebase

---
+++ js/share-btn.js
@@ -0,0 +1,20 @@
+const name = document.querySelector(".share");
+
+function share(){
+    console.log('clicked');
+//   if (navigator.share) { 
+//    navigator.share({
+//       title: 'WebShare API Demo',
+//       url: 'https://codepen.io/ayoisaiah/pen/YbNazJ'
+//     }).then(() => {
+//       console.log('Thanks for sharing!');
+//     })
+//     .catch(console.error);
+//     } else {
+//         shareDialog.classList.add('is-open');
+//     }
+};
+
+// shareButton.addEventListener('click', event => {
+//     console.log('he');
+//   });
